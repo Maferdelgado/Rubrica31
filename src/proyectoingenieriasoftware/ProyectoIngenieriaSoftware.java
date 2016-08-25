9b1f8e2ead98df67d729f8eb625ffbb9920bca39
MariferDelgado <fer1995am@gmail.com>
2016-08-25 18:18:00 -0500

    Hola Mundo

---
+++ src/proyectoingenieriasoftware/ProyectoIngenieriaSoftware.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoingenieriasoftware;
+
+import java.io.IOException;
+
+/**
+ *
+ * Autora: Mafer Delgado
+ * Cuidad:Manta
+ * Fecha:25 de Agosto del 2016
+ */
+public class ProyectoIngenieriaSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args)throws IOException{
+      
+        // TODO code application logic here      
+         Menu();
+    }
+public static void Menu()throws IOException{
+     System.out.println("\n\n\t\t\t=============Menú=================");
+     System.out.println("\t\t\t=                                    =");
+     System.out.println("\t\t\t= 1-Universidad Laica Eloy Alfaro de Manabí =");
+     System.out.println("\t\t\t= 2- Hola Ing soy Mafer                     =");
+     System.out.println("\t\t\t= 3- Salir                           =");
+     System.out.println("\t\t\t======================================");
+     System.out.println("\t\t\tOpcion: ");
+    
+    } 
+        
+    }
